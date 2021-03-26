3d7cbfa4d0a63d1a1bb5304b126f2d6be7d3b2aa
Anotso <40184359+Anotso@users.noreply.github.com>
2021-03-25 22:22:42 -0300

    First commit

---
+++ client/src/terminalController.js
@@ -0,0 +1,24 @@
+import ComponentsBuilder from "./components.js"
+
+export default class TerminalController{
+    constructor(){}
+
+    #onInputReceived(eventEmitter){
+        return function() {
+            const message = this.getValue()
+            console.log(message)
+            this.clearValue()
+        }
+    }
+
+    async initializeTable(eventEmitter){
+        const components = new ComponentsBuilder()
+            .setScreen({title: "Chat"})
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmitter))
+            .build()
+
+        components.input.focus()
+        components.screen.render()
+    }
+}
